be26b9d8de41a44c8f56251deb09101b31b8bc96
Garbua <denisgarbuza@yandex.ru>
2018-08-13 22:44:20 +0300

    v.2

--- src/main/java/controllers/ExampleController.java
+++ src/main/java/controllers/ExampleController.java
@@ -13,110 +13,152 @@ import java.util.*;
 @Controller
 @RequestMapping("/controller")
 public class ExampleController {
-	private Map <Integer, String> map = new HashMap<>();
+	private Map<Integer, String> map = new HashMap<>();
 	private String winner = "";
-	boolean flag = false;
+	private ArrayList<Integer> list = new ArrayList<>();
+	private String pl= "x";
+	private String pc = "o";
+	private int pole = 3;
 
 	@RequestMapping(value = "/player")
 	public String Player(HttpServletRequest request) {
 //Записываем в мапу состояние игрового поля (ход игрока):
-		map.put(0,request.getParameter("0"));
-		map.put(1,request.getParameter("1"));
-		map.put(2,request.getParameter("2"));
-		map.put(3,request.getParameter("3"));
-		map.put(4,request.getParameter("4"));
-		map.put(5,request.getParameter("5"));
-		map.put(6,request.getParameter("6"));
-		map.put(7,request.getParameter("7"));
-		map.put(8,request.getParameter("8"));
+		pole(request);
 //Проверка победителя:
-		winner();
+		winner(pl);
+		win();
 //Ход компьютера:
-		ai();
+		ai(request);
 //Проверка победителя:
-		winner();
-//Передача данных на игровое поле:
-		request.setAttribute("pp", map.get(0));
-		request.setAttribute("pv", map.get(1));
-		request.setAttribute("pt", map.get(2));
-		request.setAttribute("vp", map.get(3));
-		request.setAttribute("vv", map.get(4));
-		request.setAttribute("vt", map.get(5));
-		request.setAttribute("tp", map.get(6));
-		request.setAttribute("tv", map.get(7));
-		request.setAttribute("tt", map.get(8));
+		winner(pc);
+		win();
+
 		request.setAttribute("win", winner);
+		list.clear();
 
 
 		return "pages/nick";
 	}
 
-	public String winner() {
-		String user = "Победил игрок!";
-		String ai = "Победил компьютер!";
+	public String winner(String p) {
+		winner = p;
 		String n = "Ничья!";
+		// проверка горизонтали
+		for (int i =0; i < pole * pole; i+=pole) {
+			for (int j = 0; j < pole; j++) {
+				if (!map.get(i + j).equalsIgnoreCase(p)) {
+					winner = "";
+				}
+			}
+		}
 
-		if ((map.get(0).equalsIgnoreCase("x") && map.get(1).equalsIgnoreCase("x") && map.get(2).equalsIgnoreCase("x")) ||
-				(map.get(3).equalsIgnoreCase("x") && map.get(4).equalsIgnoreCase("x") && map.get(5).equalsIgnoreCase("x")) ||
-				(map.get(6).equalsIgnoreCase("x") && map.get(7).equalsIgnoreCase("x") && map.get(8).equalsIgnoreCase("x")) ||
-				(map.get(0).equalsIgnoreCase("x") && map.get(3).equalsIgnoreCase("x") && map.get(6).equalsIgnoreCase("x")) ||
-				(map.get(1).equalsIgnoreCase("x") && map.get(4).equalsIgnoreCase("x") && map.get(7).equalsIgnoreCase("x")) ||
-				(map.get(2).equalsIgnoreCase("x") && map.get(5).equalsIgnoreCase("x") && map.get(8).equalsIgnoreCase("x")) ||
-				(map.get(0).equalsIgnoreCase("x") && map.get(4).equalsIgnoreCase("x") && map.get(8).equalsIgnoreCase("x")) ||
-				(map.get(2).equalsIgnoreCase("x") && map.get(4).equalsIgnoreCase("x") && map.get(6).equalsIgnoreCase("x"))) {
-				flag = true;
-			return winner = user;
+//		for (int i =0; i < pole * pole; i+=pole){
+//				for(int j = 0; j < pole; j++)
+//					if(!map.get(i+j).equalsIgnoreCase(p)){
+//						 winner = "";
+//					}
+//			}
+//		//проверка вертикали
+		for (int i =0; i < pole * pole; i+=pole) {
+			for (int j = 0; j < pole; j++) {
+					if (!map.get((i/pole) + (j * pole) ).equalsIgnoreCase(p)){
+						winner = "";
+					}
+			}
+		}
 
-		} else if ((map.get(0).equalsIgnoreCase("o") && map.get(1).equalsIgnoreCase("o") && map.get(2).equalsIgnoreCase("o")) ||
-				(map.get(3).equalsIgnoreCase("o") && map.get(4).equalsIgnoreCase("o") && map.get(5).equalsIgnoreCase("o")) ||
-				(map.get(6).equalsIgnoreCase("o") && map.get(7).equalsIgnoreCase("o") && map.get(8).equalsIgnoreCase("o")) ||
-				(map.get(0).equalsIgnoreCase("o") && map.get(3).equalsIgnoreCase("o") && map.get(6).equalsIgnoreCase("o")) ||
-				(map.get(1).equalsIgnoreCase("o") && map.get(4).equalsIgnoreCase("o") && map.get(7).equalsIgnoreCase("o")) ||
-				(map.get(2).equalsIgnoreCase("o") && map.get(5).equalsIgnoreCase("o") && map.get(8).equalsIgnoreCase("o")) ||
-				(map.get(0).equalsIgnoreCase("o") && map.get(4).equalsIgnoreCase("o") && map.get(8).equalsIgnoreCase("o")) ||
-				(map.get(2).equalsIgnoreCase("o") && map.get(4).equalsIgnoreCase("o") && map.get(6).equalsIgnoreCase("o"))) {
-				flag= true;
-			return winner = ai;
-
-		} else if (!map.containsValue("")){
-					flag = true;
-					return winner = n;
-				}else {
+//		for (int k = 0; k < pole; k++){
+//			for (int i =0; i < pole * pole; i+=pole)
+//			if (!map.get(k + i).equalsIgnoreCase(p)){
+//			 winner = "";
+//			}
+//		}
+		//проверка диагонали 0-4-8
+		for (int i =0; i < pole * pole; i+=pole+1){
+			if (!map.get(i).equalsIgnoreCase(p)){
+				winner = "";
+			}
+		}
+		//проверка диагонали 2-4-6
+		for (int i = pole - 1; i< (pole*pole)-1; i+= pole -1){
+			if (!map.get(i).equalsIgnoreCase(p)){
+				winner = "";
+			}
+		}
+		//проверка ничьей
+		if (!map.containsValue("")) {
+				return winner = n;
+			}
 			return winner = "";
+
+
+
+
+
+
+
+// пределать проверку
+//		if ((map.get(0).equalsIgnoreCase("x") && map.get(1).equalsIgnoreCase("x") && map.get(2).equalsIgnoreCase("x")) ||
+//				(map.get(3).equalsIgnoreCase("x") && map.get(4).equalsIgnoreCase("x") && map.get(5).equalsIgnoreCase("x")) ||
+//				(map.get(6).equalsIgnoreCase("x") && map.get(7).equalsIgnoreCase("x") && map.get(8).equalsIgnoreCase("x")) ||
+//				(map.get(0).equalsIgnoreCase("x") && map.get(3).equalsIgnoreCase("x") && map.get(6).equalsIgnoreCase("x")) ||
+//				(map.get(1).equalsIgnoreCase("x") && map.get(4).equalsIgnoreCase("x") && map.get(7).equalsIgnoreCase("x")) ||
+//				(map.get(2).equalsIgnoreCase("x") && map.get(5).equalsIgnoreCase("x") && map.get(8).equalsIgnoreCase("x")) ||
+//				(map.get(0).equalsIgnoreCase("x") && map.get(4).equalsIgnoreCase("x") && map.get(8).equalsIgnoreCase("x")) ||
+//				(map.get(2).equalsIgnoreCase("x") && map.get(4).equalsIgnoreCase("x") && map.get(6).equalsIgnoreCase("x"))) {
+//			return winner = user;
+//
+//		} else if ((map.get(0).equalsIgnoreCase("o") && map.get(1).equalsIgnoreCase("o") && map.get(2).equalsIgnoreCase("o")) ||
+//				(map.get(3).equalsIgnoreCase("o") && map.get(4).equalsIgnoreCase("o") && map.get(5).equalsIgnoreCase("o")) ||
+//				(map.get(6).equalsIgnoreCase("o") && map.get(7).equalsIgnoreCase("o") && map.get(8).equalsIgnoreCase("o")) ||
+//				(map.get(0).equalsIgnoreCase("o") && map.get(3).equalsIgnoreCase("o") && map.get(6).equalsIgnoreCase("o")) ||
+//				(map.get(1).equalsIgnoreCase("o") && map.get(4).equalsIgnoreCase("o") && map.get(7).equalsIgnoreCase("o")) ||
+//				(map.get(2).equalsIgnoreCase("o") && map.get(5).equalsIgnoreCase("o") && map.get(8).equalsIgnoreCase("o")) ||
+//				(map.get(0).equalsIgnoreCase("o") && map.get(4).equalsIgnoreCase("o") && map.get(8).equalsIgnoreCase("o")) ||
+//				(map.get(2).equalsIgnoreCase("o") && map.get(4).equalsIgnoreCase("o") && map.get(6).equalsIgnoreCase("o"))) {
+//			return winner = ai;
+//
+//		} else if (!map.containsValue("")) {
+//			return winner = n;
+//		} else {
+//			return winner = "";
+//		}
+	}
+
+
+	public void ai(HttpServletRequest request) {
+		Random random = new Random();
+		if (winner.equals("") && (list.size() != 0)) {
+			int ai = random.nextInt(list.size());
+			map.put(list.get(ai), "o");
+			request.setAttribute("p" + list.get(ai), map.get(list.get(ai)));
+			return;
+
+		}
+	}
+
+	public void pole(HttpServletRequest request) {
+		for (int i = 0; i <= (pole * pole)-1 ; i++) {
+			String param = String.valueOf(i);
+			map.put(i, request.getParameter(param));
+			request.setAttribute("p" + i, map.get(i));
+			if (map.get(i).equals("")) {
+				list.add(i);
 			}
 
 		}
 
 
-
-public void ai() {
-	Random random = new Random();
-	Map <Integer, Integer> map2 = new HashMap<>();
-	Integer count = 1;
-	for (int a = 0; a <= 8; a++) {
-		if (map.get(a).equals("")) {
-			map2.put(count,a);
-			count++;
-		}
 	}
 
-	System.out.println( "count = " + count);
-	if ( (!map2.containsValue(""))|| flag) {
-		int j = random.nextInt(count-1)+1;
-		map.put(map2.get(j), "o");
-		return;
+	public void win(){
+		String user = "Победил игрок!";
+		String ai = "Победил компьютер!";
+		if (winner.equalsIgnoreCase(pl)){
+			winner = user;
+		}else if (winner.equalsIgnoreCase(pc)){
+			winner = ai;
+		}else winner = "";
+
 	}
-}
-
-
-
-//		for (int i = 0; i < 1 ; i--){
-//			int j = random.nextInt(8);
-//			if (map.get(j).equals("")){
-//				map.put(j,"o");
-//				return;
-//			}
-//		}
-
-
 }
--- src/main/webapp/WEB-INF/pages/nick.jsp
+++ src/main/webapp/WEB-INF/pages/nick.jsp
@@ -7,19 +7,19 @@
 <form action="/controller/player" method="get" name="player">
     <table align="center", border="2", cellspacing="2", cellpadding="10", width="50">
         <tr>
-            <td><input type="text" name="0" value="${pp}"></td>
-            <td><input type="text" name="1" value="${pv}"></td>
-            <td><input type="text" name="2" value="${pt}"></td>
+            <td><input type="text" name="0" value="${p0}"></td>
+            <td><input type="text" name="1" value="${p1}"></td>
+            <td><input type="text" name="2" value="${p2}"></td>
         </tr>
         <tr>
-            <td><input type="text" name="3" value="${vp}"></td>
-            <td><input type="text" name="4" value="${vv}"></td>
-            <td><input type="text" name="5" value="${vt}"></td>
+            <td><input type="text" name="3" value="${p3}"></td>
+            <td><input type="text" name="4" value="${p4}"></td>
+            <td><input type="text" name="5" value="${p5}"></td>
         </tr>
         <tr>
-            <td><input type="text" name="6" value="${tp}"></td>
-            <td><input type="text" name="7" value="${tv}"></td>
-            <td><input type="text" name="8" value="${tt}"></td>
+            <td><input type="text" name="6" value="${p6}"></td>
+            <td><input type="text" name="7" value="${p7}"></td>
+            <td><input type="text" name="8" value="${p8}"></td>
         </tr>
 
     </table>
